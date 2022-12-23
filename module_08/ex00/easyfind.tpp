4e013354e67cc396640d2a83f32b3e650214c530
hack10101101 <kevin.dinocera@protonmail.com>
2022-12-23 18:23:40 +0100

    first

--- module_08/ex00/easyfind.tpp
+++
@@ -1,8 +0,0 @@
-//
-// Created by Kevin Di nocera on 12/14/22.
-//
-
-#ifndef CPP_EASYFIND_TPP
-#define CPP_EASYFIND_TPP
-
-#endif //CPP_EASYFIND_TPP
--- module_08/ex02/main.cpp
+++ module_08/ex02/main.cpp
@@ -14,7 +14,6 @@ int main()
 	mstack.push(3);
 	mstack.push(5);
 	mstack.push(737);
-//[...]
 	mstack.push(0);
 	MutantStack<int>::iterator it = mstack.begin();
 	MutantStack<int>::iterator ite = mstack.end();
